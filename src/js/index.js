6163decb03d230dc17bf7cec58d9880d63c9016f
Kevin Lindström <0007keli@res.ludvika.intra>
2018-10-17 08:51:22 +0200

    löst uppgiften

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,35 @@
 /* eslint-disable no-unused-vars */
 import style from '../css/style.css';
 /* eslint-enable no-unused-vars */
+import password from './config';
+
+const connection = new WebSocket('ws://104.248.143.87:1337');
+
+connection.onopen = () => {
+  console.log('uppkoppling startad...');
+};
+connection.onerror = error => {
+  console.log(`Error: ${error}`);
+};
+connection.onmessage = message => {;
+  const textEl = document.getElementById('logger');
+  const obj = JSON.parse(message.data);
+  if (obj.type = 'heartbeat'){
+textEl.textcontent += `${obj.data}, `;
+  }
+console.log(message);
+connection.onclose = () => {
+  console.log('uppkoppling nedstängd...');
+};
+
+const btnEl = document.getElementById('btn');
+const textEl = document.getElementById('logger');
+btnEl.addEventListener('click', () => {
+  const obj = {
+    type: 'heartbeat',
+    data: '',
+    key: password
+  };
+  const jsonObj = JSON.stringify(obj);
+  connection.send(jsonObj);
+});
